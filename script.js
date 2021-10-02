1af4dc6b7469fa389938bb5e4d567d602f1d5d27
Chirag <whyiswhen@gmail.com>
2021-10-02 12:24:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 fetch("https://random-words-api.vercel.app/word")
-  .then(data => data.json())
-  .then(wordData=> {
-      const wordt = wordData.word;
-      const definitiont = wordData.definition ;
-      const pronunciationt = wordData.pronunciation ;
+  .then((data) => data.json())
+  .then((wordData) => {
+    const wordt = wordData[0].word;
+    const definitiont = wordData[0].definition;
+    const pronunciationt = wordData[0].pronunciation;
 
+    const wordp = document.getElementById("wordp");
+    const definitionp = document.getElementById("definitionp");
+    const pronunciationp = document.getElementById("pronunciationp");
 
-      const wordp = document.getElementById('wordp')
-      const definitionp = document.getElementById('definitionp')
-      const pronunciationp = document.getElementById('pronunciationp')
-
-      wordp.innerHTML = wordt;
-      definitionp.innerHTML = definitiont;
-      pronunciationp.innerHTML = pronunciationt;
-  
-  })
+    wordp.innerHTML = wordt;
+    definitionp.innerHTML = definitiont;
+    pronunciationp.innerHTML = pronunciationt;
+  });
